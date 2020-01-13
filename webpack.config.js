3a9af02102b0a140151c797b3897d38c51d19cc4
Deniss Fjodorovs <3starblaze@gmail.com>
2020-01-13 07:34:10 +0200

    Configure project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
